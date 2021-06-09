73bb53593fadbb6d887c8c64f260305c7ed702d0
jackychan@virtuosity-global.com <jackychan@virtuosity-global.com>
2021-06-09 11:59:26 +0800

    create mongodb in k8s, connecting to mongodb, add user model, user creation and hashed password

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'express-async-errors';
+import mongoose from 'mongoose';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -16,12 +17,26 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
+app.use(errorHandler);
+
 app.all('*', async (req, res) => {
   throw new NotFoundError();
 });
 
-app.use(errorHandler);
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('connected to mongodb');
+  } catch (err) {
+    console.error(err);
+  }
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!');
+  });
+};
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!');
-});
+start();
