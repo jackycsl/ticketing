63eb1c4baa9ed283629b3178ee8f85771950a431
jackychan@virtuosity-global.com <jackychan@virtuosity-global.com>
2021-06-14 15:41:16 +0800

    create next js client, bootstrap, add signup, signin form

---
+++ client/api/build-client.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const buildClient = ({ req }) => {
+  // on server
+  if (typeof window === 'undefined') {
+    return axios.create({
+      baseURL:
+        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      headers: req.headers,
+    });
+  } else {
+    // on browser
+    return axios.create({
+      baseURL: '/',
+    });
+  }
+};
+
+export default buildClient;
