bf85b28e4a98c7df77988511ed69410e944e0db9
jackychan@virtuosity-global.com <jackychan@virtuosity-global.com>
2021-06-24 21:55:47 +0800

    create order service

---
+++ orders/src/models/ticket.ts
@@ -0,0 +1,65 @@
+import mongoose from 'mongoose';
+import { Order, OrderStatus } from './order';
+
+interface TicketAttrs {
+  title: string;
+  price: number;
+}
+
+export interface TicketDoc extends mongoose.Document {
+  title: string;
+  price: number;
+  isReserved(): Promise<Boolean>;
+}
+
+interface TicketModel extends mongoose.Model<TicketDoc> {
+  build(attrs: TicketAttrs): TicketDoc;
+}
+
+const ticketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+ticketSchema.statics.build = (attrs: TicketAttrs) => {
+  return new Ticket(attrs);
+};
+ticketSchema.methods.isReserved = async function () {
+  // Make sure that this ticket is not already reserved
+  // Run query to look at all orders, Find an order where the ticket
+  // is the ticket we just found *and* the order status is *not* cancelled
+  // If we find an order from that means the ticket *is* reserved
+  const existingOrder = await Order.findOne({
+    ticket: this.id,
+    status: {
+      $in: [
+        OrderStatus.Created,
+        OrderStatus.AwaitingPayment,
+        OrderStatus.Complete,
+      ],
+    },
+  });
+
+  return !!existingOrder;
+};
+
+const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema);
+
+export { Ticket };
