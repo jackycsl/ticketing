14f9d7cbd7fc65c568e2c0a3d37065f91a5421a4
jackychan@virtuosity-global.com <jackychan@virtuosity-global.com>
2021-06-24 15:07:12 +0800

    implement ticket create and update NATS client publisher with test

---
+++ tickets/src/events/publishers/ticket-updated-publisher.ts
@@ -0,0 +1,5 @@
+import { Publisher, Subjects, TicketUpdatedEvent } from '@csltickets/common';
+
+export class TicketUpdatedPublisher extends Publisher<TicketUpdatedEvent> {
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+}
