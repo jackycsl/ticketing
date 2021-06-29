79ddf27337c90477c4bceb8460971c2881d1aecd
jackychan@virtuosity-global.com <jackychan@virtuosity-global.com>
2021-06-29 17:00:13 +0800

    create payment service with integration with stripe

---
+++ payments/src/app.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import 'express-async-errors';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@csltickets/common';
+import { createChargeRouter } from './routes/new';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+app.use(currentUser);
+app.use(createChargeRouter);
+
+app.use(errorHandler);
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+export { app };
---
+++ payments/src/events/__test__/order-cancelled-listener.test.ts
@@ -0,0 +1,51 @@
+import mongoose from 'mongoose';
+import { OrderCancelledEvent, OrderStatus } from '@csltickets/common';
+import { natsWrapper } from '../../nats-wrapper';
+import { OrderCancelledListener } from '../listeners/order-cancelled-listener';
+import { Order } from '../../models/order';
+
+const setup = async () => {
+  const listener = new OrderCancelledListener(natsWrapper.client);
+
+  const order = Order.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    status: OrderStatus.Created,
+    price: 10,
+    userId: 'djfkdf',
+    version: 0,
+  });
+  await order.save();
+
+  const data: OrderCancelledEvent['data'] = {
+    id: order.id,
+    version: 1,
+    ticket: {
+      id: 'dfjds',
+    },
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, order, data, msg };
+};
+
+it('update the status of the order', async () => {
+  const { listener, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  const updatedOrder = await Order.findById(data.id);
+
+  expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});
+
+it('acks the message', async () => {
+  const { listener, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  expect(msg.ack).toHaveBeenCalled();
+});
