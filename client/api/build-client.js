cdf845f0dd58ba47d93429eab3f1392af08e8aa2
jackychan@virtuosity-global.com <jackychan@virtuosity-global.com>
2021-07-01 14:04:12 +0800

    update client base url

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,7 +4,7 @@ const buildClient = ({ req }) => {
   // on server
   if (typeof window === 'undefined') {
     return axios.create({
-      baseURL: 'www.ticketing-app-prod-jacky.xyz',
+      baseURL: 'http://www.ticketing-app-prod-jacky.xyz/',
       headers: req.headers,
     });
   } else {
